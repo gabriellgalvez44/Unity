3b8afee06b863c3cd194a4f9022236575adf7e9a
gabriellgalvez44 <gabriellgalvez44@gmail.com>
2025-08-02 19:02:06 +0000

    Library fix 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function loadScripts() {
 const names = ["Accelerator", "AlwaysMove", "Audio", "AutoDestroy", "Beam", "BotNavigation", "Buttons", "CameraCanvas2D", "CameraControl", "CameraRotator", "Checkpoint", "ChildObjects", "CollectableManagement", "Collectables", "ComponentManager", "Cycle", "Date", "Delay", "DestroyZone", "Drag", "EventTrigger", "ExtendedControls", "Follow", "GamControl", "Interactive", "IsWatching", "Mode", "ObjectManagement", "OpenUI", "OverrideChildObjects", "Pathway", "Performance", "PhysicsManager", "PlayerMovement", "RandomTrigger", "Rotate", "Rotating", "Spawner", "Split", "SpotLight", "StatComparator", "StatCreator", "StateTrigger", "StatOverrider", "Teleport", "TimeOfDay", "Timer", "TimerManager", "TouchTrigger", "Trigger", "UIManagement", "VariableOverride", "VoiceInput", "VoiceListener"]
 names.forEach(collectedName => {
 create(`<div class=cs>
-<button onclick="window.location.href = '${collectedName}.html'">
+<button onclick="window.location.href = './Scripts/${collectedName}.html'">
 ${collectedName.replace("Gam", "Game")}.cs
 </button>
 </div>`, "scr")
